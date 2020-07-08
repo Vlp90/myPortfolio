3e9fc8cd99f95a311337749d2aa8afa5ffcafde8
Vlp90 <vladimir.leloup@gmail.com>
2020-07-08 15:44:52 +0200

    index: get all from db done

--- myPortfolio/bin/seeds.js
+++ myPortfolio/bin/seeds.js
@@ -36,23 +36,23 @@ const User = require("../models/User");
 // WORK SEEDS
 
 const myWork = [
-  //   {
-  //     title: "Sportify",
-  //     description: "First App to be printed to my awesome Portfolio",
-  //     imagePreview:
-  //       "https://en.webself.net/img/accueil-anim/appareil-architecte_en.png",
-  //     dateCreation: "2020-06-02",
-  //     tag: ["Full stack",
-  //     "Front-end",
-  //     "Back-end"],
-  //     video: "https://www.youtube.com/watch?v=VkDaa_7npz8&feature=youtu.be",
-  //     imgOne:
-  //       "https://99designs-start-attachments.imgix.net/alchemy-pictures/2016%2F01%2F29%2F06%2F52%2F16%2F2574220d-7737-4c39-a562-8e153a5d1497%2Fwebsite-hero3.png?auto=format&ch=Width%2CDPR&fm=png&w=824&h=457",
-  //     imgTwo:
-  //       "https://99designs-start-attachments.imgix.net/alchemy-pictures/2016%2F01%2F29%2F06%2F52%2F11%2Fdd7e48e3-a2d3-471d-9038-713f265278d1%2Fwebsite-hero1.png?auto=format&ch=Width%2CDPR&fm=png&w=824&h=457",
-  //     imgThree:
-  //       "https://99designs-start-attachments.imgix.net/alchemy-pictures/2016%2F01%2F29%2F06%2F52%2F17%2Fca5d989e-beb8-4fd0-887e-4d74a280d273%2Fwebsite-hero2.png?auto=format&ch=Width%2CDPR&fm=png&w=824&h=457",
-  //   },
+    {
+      title: "Sportify",
+      description: "First App to be printed to my awesome Portfolio",
+      imagePreview:
+        "https://en.webself.net/img/accueil-anim/appareil-architecte_en.png",
+      dateCreation: "2020-06-02",
+      tag: ["Full stack",
+      "Front-end",
+      "Back-end"],
+      video: "https://www.youtube.com/watch?v=VkDaa_7npz8&feature=youtu.be",
+      imgOne:
+        "https://99designs-start-attachments.imgix.net/alchemy-pictures/2016%2F01%2F29%2F06%2F52%2F16%2F2574220d-7737-4c39-a562-8e153a5d1497%2Fwebsite-hero3.png?auto=format&ch=Width%2CDPR&fm=png&w=824&h=457",
+      imgTwo:
+        "https://99designs-start-attachments.imgix.net/alchemy-pictures/2016%2F01%2F29%2F06%2F52%2F11%2Fdd7e48e3-a2d3-471d-9038-713f265278d1%2Fwebsite-hero1.png?auto=format&ch=Width%2CDPR&fm=png&w=824&h=457",
+      imgThree:
+        "https://99designs-start-attachments.imgix.net/alchemy-pictures/2016%2F01%2F29%2F06%2F52%2F17%2Fca5d989e-beb8-4fd0-887e-4d74a280d273%2Fwebsite-hero2.png?auto=format&ch=Width%2CDPR&fm=png&w=824&h=457",
+    },
   {
     title: "Edufy",
     description: "Second App to be printed to my awesome Portfolio",
--- myPortfolio/routes/index.js
+++ myPortfolio/routes/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const Work = require("../models/Work");
+
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  Work.find({})
+    .then((dbResult) => {
+      res.render("index", { allWork: dbResult });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 router.get("/work/:id", (req, res, next) => {
