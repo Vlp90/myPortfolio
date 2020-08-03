0f9d4da069c6a7cae5c40649e55055f3570580f7
Vlp90 <vladimir.leloup@gmail.com>
2020-08-03 12:28:11 +0200

    boutons de redirection responsive

---
+++ myPortfolio/public/javascripts/javascript.js
@@ -0,0 +1,23 @@
+// DATE
+
+// var date = new Date();
+
+// var day = date.getDate();
+// var month = date.getMonth() + 1;
+// var year = date.getFullYear();
+
+// if (month < 10) month = "0" + month;
+// if (day < 10) day = "0" + day;
+
+// var today = year + "-" + month + "-" + day;       
+// document.getElementById("creationDate").value = today;
+
+
+// for (var i = 0; i < e.target.files.length; i++) { 
+//     var file = e.target.files[i];
+//  }
+
+//  document.getElementById('inp_img').value = dataURL;
+// document.getElementById('inp_img').value += dataURL + '|';
+
+
