05fef6f90ccab63e111ba0dfd87df5e732b29799
Vlp90 <vladimir.leloup@gmail.com>
2020-08-05 15:46:06 +0200

    no display if no image

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -19,11 +19,52 @@
 //  document.getElementById('inp_img').value = dataURL;
 // document.getElementById('inp_img').value += dataURL + '|';
 
-let testVideoLink = document.getElementById("iframeTest").src;
+// IF VIDEO SRC IS EMPTY
+
+let videoLink = document.getElementById("iframeLink").src;
 let divVideo = document.getElementById("iframeDiv");
 
-if (testVideoLink.includes("work")) {
+if (videoLink.includes("work")) {
   divVideo.style.display = "none";
 }
+console.log(videoLink);
 
-console.log(testVideoLink);
+// IF IMAGE SRC IS EMPTY
+
+// 1
+let imageLink1 = document.getElementById("imageLink1").src;
+let imageDiv1 = document.getElementById("imageDiv1");
+
+if (imageLink1.includes("work")) {
+  imageDiv1.style.display = "none";
+}
+
+// 2
+let imageLink2 = document.getElementById("imageLink2").src;
+let imageDiv2 = document.getElementById("imageDiv2");
+
+if (imageLink2.includes("work")) {
+  imageDiv2.style.display = "none";
+}
+
+// 3
+let imageLink3 = document.getElementById("imageLink3").src;
+let imageDiv3 = document.getElementById("imageDiv3");
+
+if (imageLink3.includes("work")) {
+  imageDiv3.style.display = "none";
+}
+
+// 4
+let imageLink4 = document.getElementById("imageLink4").src;
+let imageDiv4 = document.getElementById("imageDiv4");
+
+if (imageLink4.includes("work")) {
+  imageDiv4.style.display = "none";
+}
+
+
+console.log(imageLink1);
+console.log(imageLink2);
+console.log(imageLink3);
+console.log(imageLink4);
